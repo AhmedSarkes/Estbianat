fc53d4a75c60c0e075e0fe1c0ce7b8aaedb29a41
sarkies <ahmed.sarkes@gmail.com>
2022-09-22 03:43:08 +0200

    Finish The Index Page & Home Page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+$(() => {
+    "use strict";
+    // Fixed Navbar
+    $(this).scroll(function () {
+        if ($(this).scrollTop() >= 50) {
+            $('.navbar').addClass('navbar-fixed');
+        } else {
+            $('.navbar').removeClass('navbar-fixed');
+        }
+    });
+    // Start Swiper
     var swiper = new Swiper(".mySwiper", {
         spaceBetween: 30,
         centeredSlides: true,
@@ -14,3 +25,39 @@
         prevEl: ".swiper-button-prev",
         },
     });
+    // End Swiper
+    // Start Question
+    $('.question').click(function () {
+        $(this).next().slideToggle();
+        $(this).parent().toggleClass('active');
+    });
+    // End Question
+    // Start Swiper
+    var swiper = new Swiper(".mySwiper-company", {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        breakpoints: {
+            390: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+            },
+            640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+            },
+            768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+            },
+            1024: {
+            slidesPerView: 5,
+            spaceBetween: 50,
+            },
+        },
+        });
+    // End Swiper
+});
