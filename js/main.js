39e67a57ba80d7f7bf1d06d72d918593ca2c4027
sarkies <ahmed.sarkes@gmail.com>
2022-09-23 23:41:13 +0200

    New Editing In Index.html & Home.html

--- js/main.js
+++ js/main.js
@@ -8,56 +8,10 @@ $(() => {
             $('.navbar').removeClass('navbar-fixed');
         }
     });
-    // Start Swiper
-    var swiper = new Swiper(".mySwiper", {
-        spaceBetween: 30,
-        centeredSlides: true,
-        autoplay: {
-        delay: 5000,
-        disableOnInteraction: false,
-        },
-        pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        },
-        navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-        },
-    });
-    // End Swiper
     // Start Question
     $('.question').click(function () {
         $(this).next().slideToggle();
         $(this).parent().toggleClass('active');
     });
     // End Question
-    // Start Swiper
-    var swiper = new Swiper(".mySwiper-company", {
-        slidesPerView: 1,
-        spaceBetween: 10,
-        autoplay: {
-            delay: 2500,
-            disableOnInteraction: false,
-        },
-        breakpoints: {
-            390: {
-            slidesPerView: 1,
-            spaceBetween: 10,
-            },
-            640: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-            },
-            768: {
-            slidesPerView: 4,
-            spaceBetween: 40,
-            },
-            1024: {
-            slidesPerView: 5,
-            spaceBetween: 50,
-            },
-        },
-        });
-    // End Swiper
 });
