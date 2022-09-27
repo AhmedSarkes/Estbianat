5b7f974c6f5ed8735b4560ae753360510c597524
sarkies <ahmed.sarkes@gmail.com>
2022-09-27 23:51:52 +0200

    New Editing & New Creation In pages/register.html & pages/question.html & pages/articles.html & pages/article.html & pages/add-survey.html & pages/add-services-research.html & pages/send-code.html & pages/register-2.html & pages/login.html & pages/home.html & pages/forget-pw.html & pages/conditions.html & pages/alert.html & js/main.js & index.html & css/style.css

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(() => {
   "use strict";
   // Fixed Navbar
-  $(this).scroll(function () {
+  $(this).scroll(() => {
     if ($(this).scrollTop() >= 50) {
       $(".navbar").addClass("navbar-fixed");
     } else {
@@ -9,20 +9,20 @@ $(() => {
     }
   });
   // Start Question
-  $(".question").click(function () {
+  $(".question").click(() => {
     $(this).next().slideToggle();
     $(this).parent().toggleClass("active");
   });
   // End Question
   //   Start Dropdown Menu
-  $(".dropdown").on("mouseover", function () {
+  $(".dropdown").on("mouseover", () => {
     if ($(".dropdown")) {
       $(".dropdown .profile + .dropdown-menu").addClass(
         "d-flex justify-content-center align-content-end flex-column text-end show"
       );
     } else return;
   });
-  $(".dropdown").on("mouseleave", function () {
+  $(".dropdown").on("mouseleave", () => {
     if ($(".dropdown")) {
       $(".dropdown .profile + .dropdown-menu").removeClass(
         "d-flex justify-content-center align-content-end flex-column text-end show"
@@ -31,81 +31,215 @@ $(() => {
   });
   //   End Dropdown Menu
   // Start Show Password
-  $('.fa-eye-slash').on('click', function () {
-    $('.fa-eye-slash').css({
-      display: 'none',
+  $(".fa-eye-slash").on("click", () => {
+    $(".fa-eye-slash").css({
+      display: "none",
     });
-    $('.fa-eye-slash + .fa-eye').css({
-      display: 'flex',
+    $(".fa-eye-slash + .fa-eye").css({
+      display: "flex",
     });
-    $('#password').attr('type', 'text');
-    $('#confirm-password').attr('type', 'text');
-  })
-  $('.fa-eye').on('click', function () {
-    $('.fa-eye').css({
-      display: 'none',
+    $("#password").attr("type", "text");
+    $("#confirm-password").attr("type", "text");
+  });
+  $(".fa-eye").on("click", () => {
+    $(".fa-eye").css({
+      display: "none",
     });
-    $('.fa-eye-slash').css({
-      display: 'flex',
+    $(".fa-eye-slash").css({
+      display: "flex",
     });
-    $('#password').attr('type', 'password');
-    $('#confirm-password').attr('type', 'password');
-  })
+    $("#password").attr("type", "password");
+    $("#confirm-password").attr("type", "password");
+  });
   // End Show Password
   // Start Tab Class
   // Start Student
-  $('.student #student').on('click', function () {
-    $('.container-student#student').css({
-      display: 'block',
+  $(".student #student").on("click", () => {
+    $(".container-student#student").css({
+      display: "block",
     });
-    $('.container-employer#employer').css({
-      display: 'none',
+    $(".container-employer#employer").css({
+      display: "none",
     });
-    $('.container-graduate#graduate').css({
-      display: 'none',
+    $(".container-graduate#graduate").css({
+      display: "none",
     });
   });
   // End Student
   // Start Employer
-  $('.employer #employer').on('click', function () {
-    $('.container-employer#employer').css({
-      display: 'block',
+  $(".employer #employer").on("click", () => {
+    $(".container-employer#employer").css({
+      display: "block",
     });
-    $('.container-student#student').css({
-      display: 'none',
+    $(".container-student#student").css({
+      display: "none",
     });
-    $('.container-graduate#graduate').css({
-      display: 'none',
+    $(".container-graduate#graduate").css({
+      display: "none",
     });
   });
   // End Employer
   // Start Graduate
-  $('.graduate #graduate').on('click', function () {
-    $('.container-graduate#graduate').css({
-      display: 'block',
+  $(".graduate #graduate").on("click", () => {
+    $(".container-graduate#graduate").css({
+      display: "block",
     });
-    $('.container-employer#employer').css({
-      display: 'none',
+    $(".container-employer#employer").css({
+      display: "none",
     });
-    $('.container-student#student').css({
-      display: 'none',
+    $(".container-student#student").css({
+      display: "none",
     });
   });
+  // End Graduate
   // End Tab Class
+  // Start Open Popup Survey
+  $(".survey-btn-index").on("click", () => {
+    $(".popup-overlay").css({
+      display: "block",
+    });
+    $(".container-survey").css({
+      display: "block",
+    });
+  });
+  // End Open Popup Survey
+  // Start Close Popup Survey
+  $(".popup-overlay").on("click", (event) => {
+    if (event.target.class !== $(".container-survey")) {
+      $(".popup-overlay").css({
+        display: "none",
+      });
+      $(".container-survey").css({
+        display: "none",
+      });
+    } else {
+      $(".popup-overlay").css({
+        display: "block",
+      });
+    }
+  });
+  // End Close Popup Survey
+  // Start Remove Alert
+  $(".container-alert .delete .fa-trash-can").on("click", () => {
+    $(".feedback-alert").remove();
+  });
+  $(".container-alert .delete .fa-trash-can").on("click", () => {
+    $(".feedback-alert").remove();
+  });
+  $(".container-alert .delete .fa-trash-can").on("click", () => {
+    $(".feedback-alert").remove();
+  });
+  // End Remove Alert
+  // Start Open Popup Feedback Alert
+  $(".feedback-alert").on("click", () => {
+    $(".popup-overlay").css({
+      display: "block",
+    });
+    $(".container-feedback").css({
+      display: "block",
+    });
+  });
+  // End Open Popup Feedback Alert
+  // Start Close Popup Feedback Alert
+  $(".popup-overlay").on("click", (event) => {
+    if (event.target.class !== $(".container-feedback")) {
+      $(".popup-overlay").css({
+        display: "none",
+      });
+      $(".container-feedback").css({
+        display: "none",
+      });
+    }
+  });
+  // End Close Popup Feedback Alert
+  // Start Open Popup Feedback Alert
+  $(".send-feedback-btn").on("click", () => {
+    $(".popup-overlay").css({
+      display: "block",
+    });
+    $(".container-success-feedback").css({
+      display: "block",
+    });
+    $(".container-feedback").css({
+      display: "none",
+    });
+  });
+  // End Open Popup Feedback Alert
+  // Start Close Popup Feedback Alert
+  $(".popup-overlay").on("click", (event) => {
+    if (event.target.class !== $(".container-success-feedback")) {
+      $(".popup-overlay").css({
+        display: "none",
+      });
+      $(".container-success-feedback").css({
+        display: "none",
+      });
+    }
+  });
+  // End Close Popup Feedback Alert
+  // Start Tab Class
+  // Start Statistical
+  $(".container-statistical #statistical").on("click", () => {
+    $(".container-statistical#statistical").css({
+      display: "block",
+    });
+    $(".container-judging#judging").css({
+      display: "none",
+    });
+    $(".container-accuracy#accuracy").css({
+      display: "none",
+    });
+  });
+  // End Statistical
+  // Start Judging
+  $(".container-judging #judging").on("click", () => {
+    $(".container-judging#judging").css({
+      display: "block",
+    });
+    $(".container-statistical#statistical").css({
+      display: "none",
+    });
+    $(".container-accuracy#accuracy").css({
+      display: "none",
+    });
+  });
+  // End Judging
+  // Start Accuracy
+  $(".container-accuracy #accuracy").on("click", () => {
+    $(".container-accuracy#accuracy").css({
+      display: "block",
+    });
+    $(".container-statistical#statistical").css({
+      display: "none",
+    });
+    $(".container-judging#judging").css({
+      display: "none",
+    });
+  });
+  // End Accuracy
+  // End Tab Class
+  // Start Send Order
+  // Start Open Send Order Popup Overlay
+  $(".send-order-btn").on("click", () => {
+    $(".popup-overlay").css({
+      display: "block",
+    });
+    $(".container-success-send-order").css({
+      display: "block",
+    });
+  });
+  // End Open Send Order Popup Overlay
+  // Start Close Send Order Popup Overlay
+  $(".popup-overlay").on("click", (event) => {
+    if (event.target.class !== $(".container-success-send-order")) {
+      $(".popup-overlay").css({
+        display: "none",
+      });
+      $(".container-success-send-order").css({
+        display: "none",
+      });
+    }
+  });
+  // End Close Send Order Popup Overlay
+  // End Send Order
 });
-
-// document.addEventListener('click', e => {
-//     const isDropdownButton = e.target.matches("[data-dropdown-button]");
-//     if (!isDropdownButton && e.target.closest('[data-dropdown]') != null) return;
-
-//     let currentDropdown;
-//     if (isDropdownButton) {
-//         currentDropdown = e.target.closest('[data-dropdown]');
-//         currentDropdown.classList.toggle('active');
-//     }
-
-//     document.querySelectorAll("[data-dropdown].active").forEach( dropdown => {
-//         if (dropdown === currentDropdown) return;
-//         dropdown.classList.remove('active');
-//     });
-// });
